fd094f986404c6f033207a7e88569e9f7779bf20
Caio Augusto Barros Costa <caioabc2000@gail.com>
2025-09-02 17:43:37 -0300

    projeto-java

---
+++ programa-sequencial.js
@@ -0,0 +1,6 @@
+//## Centimetros para polegadas]
+//Converter cm -> in (1 in = 2,54).
+//Elabore um programa que leia um valor em **centimentros** e converta para **polegadas**.
+//Considere a fórmula:
+//`1 polegada = 2,54 centimetros`
+//Mostre o resultado com **duas casas decimais**
