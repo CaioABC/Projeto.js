43c609ce18e56d4d06301520136444ba50b62f4d
Caio Augusto Barros Costa <caioabc2000@gail.com>
2025-09-05 17:42:51 -0300

    projeto.js

--- programa-sequencial.js
+++ programa-sequencial.js
@@ -1,16 +1,53 @@
+//ativ 1. Básico
 
-
-//Exemplo 3
-//Nota(aprovado ou reprovado)
-
-const nota = Number(prompt("Nota (0-100):"));
-
-if (Number.isNaN(nota) || nota < 0 || nota > 100) {
-    console.log("Nota invalida")
-} else if (nota < 60) {
-    console.log("Reprovado")
-} else if (nota < 80) {
-    console.log("Recuperação")
-}else {
-    console.log("Aprovado")
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
 }
+
+
+//ativ 2.Básico
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`1 x ${i} = ${1 * i} `)
+}
+
+
+//ativ 3.Básico
+
+let N = Number(prompt("Digite um número: "));
+
+let soma = 0;
+
+for (let i = 1; i <= N; i++) {
+  soma += i;
+}
+console.log(`O total dos números de 1 a ${N} é: ${soma}`);
+
+//Ativ 1. Médio
+
+for (let i = 2; i <= 50; i += 2) {
+    console.log(i);
+}
+
+
+
+//Ativ 2. Médio
+
+let secreto = Math.floor(Math.random() * 100) + 1;
+
+let tentativas;
+
+while (tentativas !== secreto) {
+    tentativas = Number(prompt("Adivinhe o número entre 1 e 100: "))
+
+    if (tentativas > secreto) {
+        alert("Tente um número menor!")
+    } else if (tentativas < secreto) {
+        alert("Tente um número maior")
+    } else {
+        alert("Parabéns, você acertou!!!!!")
+    }
+}
+
+
