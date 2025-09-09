ed17537e6289813d943b2d653bcc2706d087c939
Caio Augusto Barros Costa <caioabc2000@gail.com>
2025-09-09 17:42:07 -0300

    Projeto.js

--- programa-sequencial.js
+++ programa-sequencial.js
@@ -1,12 +1,42 @@
-//Filter, Map, Find e Reduce
+// const nums = [10, 15, 20, 25, 30];
+// const nums2 = nums.filter(numero => numero >= 20);
+// console.log(nums2);
 
-const compras = [
-  {nome: "Leite", preco: 5},
-  {nome: "Pão", preco: 8},
-  {nome: "Manteiga", preco: 12},
-]
-const valorTotal = compras.reduce((acumulador, itemAtual) => {
-  return acumulador + itemAtual.preco
-}, 0)
+// const palavras = ["js", "html"];
+// const maiusculas = palavras.map(palavra => palavra.toUpperCase());
+// console.log(maiusculas);
 
-console.log(valorTotal)
+// const usuarios = [
+//   {id: 1, nome: "Caio"},
+//   {id: 2, nome: "Ana"},
+//   {id: 3, nome: "Bruno"},
+// ];
+// const usuario1 = usuarios.find(usuario => usuario.id === 1);
+// console.log(usuario1);
+
+// const produtos = [
+//   { nome: "Camiseta", preco: 12 },
+//   { nome: "Tênis", preco: 35 },
+// ];
+// const nomes = produtos.filter(p => p.preco > 20).map(p => p.nome);
+
+// console.log(nomes);
+
+// const itens = [{ preco: 10 }, { preco: 20 }, { preco: 5 }];
+// const precoTotal = itens.reduce((acumulador, itemAtual) => acumulador + itemAtual.preco, 0);
+
+// console.log(precoTotal);
+
+// const items = [
+//   { nome: "Caneta", categoria: "papel", preco: 3 },
+//   { nome: "Caderno", categoria: "papel", preco: 15 },
+//   { nome: "Banana", categoria: "alimento", preco: 2 },
+// ];
+
+// const total = items.reduce((acc, it) => {
+//   const cat = it.categoria;
+//   if (!acc[cat]) acc[cat] = 0;
+//   acc[cat] += it.preco;
+//   return acc;
+// }, {});
+// console.log(total);
